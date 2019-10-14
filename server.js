d0a1294ac49257682313deff5d0ecbe3d9b07d97
ShawnG <shawngway@gmail.com>
2019-10-14 18:04:47 -0400

    grabbing html information for js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function () {
